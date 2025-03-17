2626945c79169bc5514a6a3c25ce37705fe4b322
Fabricio Souza <banksapk@gmail.com>
2025-03-16 22:19:15 -0300

    correcting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://seu-frontend.onrender.com'],
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
